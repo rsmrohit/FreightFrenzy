a28c557e34af76b88830adcd4ebf8c871bb4bc86
Rohit <rohmam060405@gmail.com>
2021-10-09 20:52:05 -0700

    Added Basic Class files for simple motor teleop testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareFF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareFF.java
@@ -1,4 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class HardwareFF {
+
+    public DcMotor frontLeft;
+    public DcMotor backLeft;
+    public DcMotor frontRight;
+    public DcMotor backRight;
+
+    HardwareMap hwMap = null;
+
+    public void init (HardwareMap awhMap) {
+
+        hwMap = awhMap;
+
+        frontLeft = hwMap.get(DcMotor.class, "front_left");
+        frontRight = hwMap.get(DcMotor.class, "front_right");
+        backRight = hwMap.get(DcMotor.class, "back_right");
+        backLeft = hwMap.get(DcMotor.class, "back_left");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        frontRight.setDirection(DcMotor.Direction.FORWARD);// Set to FORWARD if using AndyMark motors
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Testing {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Testing", group="UltimateGoal")
+public class Testing extends OpMode {
+
+    // declaring variables
+    MecanumDriveTrain vroom;
+
+    /* Declare OpMode members. */
+    HardwareFF robot = new HardwareFF(); // use the class created to define a RoverRuckus's hardware
+
+    @Override
+    public void init() {
+        telemetry.addData("What", "Do u want");
+        telemetry.update();
+        robot.init(hardwareMap);
+        vroom = new MecanumDriveTrain(robot, gamepad1,telemetry);
+
+        telemetry.addData("Haddi", "Haddi");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        vroom.loop();
+    }
 }
